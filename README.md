d8ef370474a1b523cf138c1b36816d3f9b09b107
vicenters1 <vm.santos@campus.fct.unl.pt>
2024-04-08 23:11:53 +0100

    Updated deploy files

--- README.md
+++
@@ -1,5 +0,0 @@
-```shell
-docker build -t beermile .   
-docker tag beermile europe-west9-docker.pkg.dev/beermile-418917/beermile/website-image
-docker push europe-west9-docker.pkg.dev/beermile-418917/beermile/website-image
-```
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "picsum.photos",
         pathname: "/**",
+        loader: 'akamai',
+        path: '',
       },
     ],
   },
+  assetPrefix: './',
 };
 
 export default nextConfig;
